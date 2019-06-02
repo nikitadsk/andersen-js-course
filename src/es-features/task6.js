10407784c70ee0839f30302cae839a48bbe8ff05
nikitadsk <nikitadsk@mail.ru>
2019-06-02 13:30:15 +0300

    es-features task 6

--- src/es-features/task6.js
+++ src/es-features/task6.js
@@ -28,4 +28,20 @@ export function task6Old() {
 // Напишите реализацию функции task6Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task6New() {}
+export function task6New() {
+  function userModule() {
+    return {
+      name: 'Max',
+      value: 100,
+      role: { name: 'Admin' },
+      cases: [{ id: '1' }],
+    };
+  }
+
+  var { name, value, role, isActive, cases } = userModule();
+
+  role = role.name;
+  isActive = isActive === undefined ? false : isActive;
+
+  return [name, value, role, isActive, cases[0].id];
+}
