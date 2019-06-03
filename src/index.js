a61b626007608cd8e742b4db3ed8a95827d1066e
nikitadsk <nikitadsk@mail.ru>
2019-06-03 19:43:11 +0300

    array-practice task6

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ import { arrayDiff } from './array-practice/task2';
 import { forEachRight } from './array-practice/task3';
 import { union } from './array-practice/task4';
 import { createGenerator } from './array-practice/task5';
+import { transformArrayToNumber } from './array-practice/task6';
 
 console.log('task1 old', task1Old(10) === 500); // true
 console.log('task1 new', task1New(10) === 500); // true
@@ -109,5 +110,13 @@ console.log(generator.next());
 console.log(generator.next());
 console.log(generator.next());
 console.log(generator.next());
+
+console.log('=============');
+
+console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc + item));
+console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc + item, 10));
+console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc * item));
+console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc * item, 1));
+console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc - item));
 
 console.log('=============');
