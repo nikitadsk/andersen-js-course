09303b204557c1c3a1c72f41875784bad7e47f79
nikitadsk <nikitadsk@mail.ru>
2019-06-03 18:22:46 +0300

    array-practice task4

--- src/array-practice/task4.js
+++ src/array-practice/task4.js
@@ -7,3 +7,18 @@
  * console.log(union([5, 1, 2, 3, 3], [4, 3, 2])); -> [5, 1, 2, 3, 4]
  * console.log(union([5, 1, 3, 3, 4], [1, 3, 4])); -> [5, 1, 3, 4]
  */
+
+// eslint-disable-next-line import/prefer-default-export
+export function union(arr1, arr2) {
+  const result = [];
+
+  arr1.forEach(element => {
+    if (result.indexOf(element) === -1) result.push(element);
+  });
+
+  arr2.forEach(element => {
+    if (result.indexOf(element) === -1) result.push(element);
+  });
+
+  return result;
+}
--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ import { task10Old, task10New } from './es-features/task10';
 import { any } from './array-practice/task1';
 import { arrayDiff } from './array-practice/task2';
 import { forEachRight } from './array-practice/task3';
+import { union } from './array-practice/task4';
 
 console.log('task1 old', task1Old(10) === 500); // true
 console.log('task1 new', task1New(10) === 500); // true
@@ -92,5 +93,10 @@ console.log(arrayDiff([1, 3, 3, 4], [1, 3, '4']));
 console.log('=============');
 
 console.log(forEachRight([1, 2, 3, 4], val => console.log(val)));
+
+console.log('=============');
+
+console.log(union([5, 1, 2, 3, 3], [4, 3, 2]));
+console.log(union([5, 1, 3, 3, 4], [1, 3, 4]));
 
 console.log('=============');
