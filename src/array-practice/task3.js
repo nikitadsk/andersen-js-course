4646173ecba74fa02ba706d8c7e41d533209b393
nikitadsk <nikitadsk@mail.ru>
2019-06-09 17:11:56 +0300

    array-practice task3

--- src/array-practice/task3.js
+++ src/array-practice/task3.js
@@ -13,7 +13,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export function forEachRight(arr, fun) {
   arr
-    .concat([])
+    .slice(0, arr.length)
     .reverse()
     .forEach(fun);
 }
