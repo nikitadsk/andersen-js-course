25c1acf40c53d262901578618916a4dd10acd413
nikitadsk <nikitadsk@mail.ru>
2019-06-28 17:38:36 +0300

    task1 выполнено

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import './styles/main.css';
+import { foo, createCb } from './asynchronous-programming/task1';
 
-console.log('Hi');
+console.log('============task1==============');
+
+console.log(foo(5, createCb('cb')));
+console.log(foo(20, createCb('cb')));
+
+console.log('===============================');
