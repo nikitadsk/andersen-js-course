02f829bc1f36ae6be988de20f4658941abfd0c72
nikitadsk <nikitadsk@mail.ru>
2019-06-29 16:16:03 +0300

    task4 исправлено

--- src/asynchronous-programming/task4.js
+++ src/asynchronous-programming/task4.js
@@ -1,20 +1,16 @@
 function task4() {
-  const response = fetch('http://www.json-generator.com/api/json/get/cfQCylRjuG').then(resp =>
-    resp.json()
-  );
-
-  response.then(data => {
-    if (data.getUsersData) {
-      const response2 = fetch('http://www.json-generator.com/api/json/get/cfVGucaXPC').then(resp =>
-        resp.json()
-      );
-
-      response2.then(data2 => {
-        console.log('task4');
-        console.log(data2);
-      });
-    }
-  });
+  fetch('http://www.json-generator.com/api/json/get/cfQCylRjuG')
+    .then(resp => resp.json())
+    .then(({ getUsersData }) => {
+      if (getUsersData) {
+        fetch('http://www.json-generator.com/api/json/get/cfVGucaXPC')
+          .then(resp2 => resp2.json())
+          .then(data => {
+            console.log('task4');
+            console.log(data);
+          });
+      }
+    });
 }
 
 export default task4;
